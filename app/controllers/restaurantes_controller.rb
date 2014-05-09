7251c858b620914a16ecf01d0b831221065b28e3
gilcierweb <gilcier06@yahoo.com.br>
2014-05-08 23:32:32 -0300

    first commit crud

---
+++ app/controllers/restaurantes_controller.rb
@@ -0,0 +1,40 @@
+class RestaurantesController < ApplicationController
+  def index
+    @restaurantes = Restaurante.order :nome
+  end
+
+  def show
+    @restaurante = Restaurante.find(params[:id])
+  end
+
+  def new
+    @restaurante = Restaurante.new
+  end
+
+  def create
+    @restaurante = Restaurante.new(restaurante_params)
+    @restaurante.save
+    redirect_to(action: "show", id: @restaurante)
+  end
+
+  def edit
+    @restaurante = Restaurante.find(params[:id])
+  end
+
+  def update
+    @restaurante = Restaurante.find(params[:id])
+    @restaurante.update_attributes(restaurante_params)
+
+    redirect_to action: "show", id: @restaurante
+  end
+
+  def destroy
+    @restaurante = Restaurante.find(params[:id])
+    @restaurante.destroy
+    redirect_to action: 'index'
+  end
+
+  def restaurante_params
+    params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+  end
+end
--- test/controllers/restaurantes_controller_test.rb
+++ test/controllers/restaurantes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OlaControllerTest < ActionController::TestCase
+class RestaurantesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
